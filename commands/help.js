247427d887ba2a5f158463c69a589622e5eac8eb
Kevin Aaaquil <eshansingh2409@gmail.com>
2021-02-16 16:06:26 +0530

    ✅

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "help",
     description: "Helps confused souls",
     execute(message, args) {
-        message.channel.send("Hey Guys");
-
+        message.channel.send("Hey Guys, New Bot for fun GAMES");
+        message.channel.send("Aree maa chudi padi hai");
     }
 }
