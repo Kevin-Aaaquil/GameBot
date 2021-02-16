cd8183fd7b9d499b2da7470e8eae24da72d9d0e5
Shashank Kumar <shashankkumarthakur@gmail.com>
2021-02-16 23:26:43 +0530

    🎨 Remove .env

---
+++ main.js
@@ -0,0 +1,39 @@
+const discord = require("discord.js");
+const config = require('./config.json');
+
+const client = new discord.Client();
+
+const prefix = "*";                                                           //comment start 
+
+const fs = require("fs");
+
+client.commands = new discord.Collection();
+
+const commandFiles = fs.readdirSync("./commands/").filter(file => file.endsWith(".js"));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}                                                                        //comment end            
+
+client.once("ready", (err) => {
+    console.log("Hello Human!!");
+});
+
+client.on("message", message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === "help") {                             //Help
+        client.commands.get("help").execute(message, args);
+    }
+
+});
+
+
+
+
+
+client.login(config.token);
