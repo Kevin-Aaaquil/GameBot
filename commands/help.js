95a3a3f33edfb3e6aa353aae1bd45bcc3a6292f3
Shashank Kumar <shashankkumarthakur@gmail.com>
2021-02-16 23:23:25 +0530

    🎨 remove node_modules

--- commands/help.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "help",
-    description: "Helps confused souls",
-    execute(message, args) {
-        message.channel.send("Hey Guys, New Bot for fun GAMES");
-        message.channel.send("Aree maa chudi padi hai");
-        message.channel.send("Bakchodi");
-    }
-}
