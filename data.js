2cb16cef49cd2a13f462489b1087fd73fd388130
Shashank Kumar <shashankkumarthakur@gmail.com>
2021-02-22 23:37:56 +0530

    ✨ add a new module to initialize and pull data

---
+++ data.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
+
+let _db;
+
+module.exports = {
+  connectToServer: function (callback) {
+    MongoClient.connect(
+      process.env.MONGODB_URI,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      function (err, MongoClient) {
+        _db = MongoClient.db();
+        return callback(err);
+      },
+    );
+  },
+
+  getDb: function () {
+    return _db;
+  },
+};
