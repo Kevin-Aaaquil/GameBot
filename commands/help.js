2e262a7537d322f0d671927304c8c920cbbdd205
Shashank Kumar <shashankkumarthakur@gmail.com>
2021-02-22 23:38:56 +0530

    ⚡️ increase parameters

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,11 @@
 module.exports = {
-    name: "help",
-    description: "Helps confused souls",
-    execute(message, args) {
-        message.channel.send("Hey Guys, New Bot for fun GAMES");
-        message.channel.send("Aree maa chudi padi hai");
-        message.channel.send("Bakchodi");
-    }
-}
+  name: 'help',
+  description: 'Helps confused souls',
+  execute(message, args, data) {
+    message.channel.send(data);
+    message.channel.send('Hey Guys, New Bot for fun GAMES');
+    message.channel.send('Aree maa chudi padi hai');
+    message.channel.send('Bakchodi');
+    message.channel.send('changes krne padenge bc aur');
+  },
+};
